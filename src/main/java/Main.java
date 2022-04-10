f9ec184198d3817d449b8bdf3e2c83b806bc8b7d
Milkina Tatyana <noiravaler@gmail.com>
2022-04-10 11:02:05 +0300

    task 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,43 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
 public class Main {
 
     public static void main(String[] args) {
         List<Task> list = new ArrayList<>();
         Task task = null;
-        String commandName = "";
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-            while (!commandName.equalsIgnoreCase("quit")) {
-                System.out.println("Введите команду");
-                String[] command = reader.readLine().split(" ");
-                commandName = command[0].toLowerCase(Locale.ROOT);
+            while (true) {
                 try {
-                    switch (commandName) {
-                        case "add":
-                            String description = command[1];
-                            task = new Task(list.size(), description);
-                            list.add(task);
-                            System.out.println("Задача '" + description + "' добавлена");
-                            break;
-                        case "print":
-                            System.out.printf("%d. [%s] %s%n", task.getIndex() + 1, task.isComplete() ? "x" : " ", task.getDescription());
-                            break;
-                        case "toggle":
-                            int index = Integer.parseInt(command[1]) - 1;
-                            Task changedTask = list.get(index);
-                            changedTask.setComplete(!changedTask.isComplete());
-                            System.out.printf("Статус задачи '%s' изменен на %s%n", changedTask.getDescription(), changedTask.isComplete());
-                            break;
-                        default:
-                            throw new IllegalArgumentException();
+                    System.out.println("Введите команду");
+                    String command = reader.readLine();
+                    if (command.matches("add .*")) {
+                        String description = command.replace("add ", "");
+                        task = new Task(list.size(), description);
+                        list.add(task);
+                        System.out.println("Задача '" + description + "' добавлена");
+                    } else if (command.equals("print")) {
+                        System.out.printf("%d. [%s] %s%n", task.getIndex() + 1, task.isComplete() ? "x" : " ", task.getDescription());
+                    } else if (command.matches("toggle \\d+")) {
+                        int index = Integer.parseInt(command.replace("toggle ", "")) - 1;
+                        Task changedTask = list.get(index);
+                        changedTask.setComplete(!changedTask.isComplete());
+                        System.out.printf("Статус задачи '%s' изменен на %s%n", changedTask.getDescription(), changedTask.isComplete());
+                    } else if (command.equals("quit")) {
+                        break;
+                    } else {
+                        throw new IllegalArgumentException();
                     }
-                } catch (IllegalArgumentException | IndexOutOfBoundsException | NullPointerException e) {
+                } catch (IllegalArgumentException | NullPointerException e) {
                     System.out.println("Ошибка при вводе данных. Введите команду еще раз");
                 }
             }
